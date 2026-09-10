fc8d3ce26c14e22b4a49aa9245c60e8b198f65b1
Lucas Correia <lfpgc@softex.cin.ufpe.br>
2026-09-10 07:51:29 -0300

    FEAT: Strategies

--- src/celular_robo/estrategias.py
+++ src/celular_robo/estrategias.py
@@ -6,3 +6,26 @@
 # TODO: implemente aqui. Considere uma base comum (RotaColeta) com
 # __init_subclass__ registrando cada rota, ver Seção 2.2 (metaprogramação
 # aplicada a uma segunda hierarquia).
+
+from abc import ABC, abstractmethod
+
+
+class RotaColeta(ABC):
+    _registro_rotas = {}
+    def __init_subclass__(cls, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        RotaColeta._registro_rotas[cls.__name__] = cls
+    pass
+
+    @abstractmethod
+    def mover(self, robo):
+        ...
+
+class RotaDireta(RotaColeta):
+    def mover(self, robo):
+        raise NotImplementedError()
+        
+
+class RotaComDuplaConferencia(RotaColeta):
+    def mover(self, robo):
+        raise NotImplementedError()
